82d7fd302dd84854ccc839ca188a2f674077e2a1
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2022-08-22 21:19:17 -0300

    Lista 2 - Exerc 1 finalizado

--- Lista2A-Exe1/src/Contribuinte.java
+++ Lista2A-Exe1/src/Contribuinte.java
@@ -49,4 +49,25 @@ public class Contribuinte {
 	public String getNome() {
 		return this.nome;
 	}
+	
+	public double getAliquota() {
+		double aliquota = 0;
+		if (this.rendaAnual > 35000) {
+			aliquota = 0.3; // 30%
+		}
+		else if (this.rendaAnual >= 25001) {
+			aliquota = 0.275; // 27,5%
+		}
+		else if (this.rendaAnual >= 9001) {
+			aliquota = 0.15; // 15%
+		}
+		else if (this.rendaAnual >= 4001) {
+			aliquota = 0.058; // 5,8%
+		}
+		return aliquota;
+	}
+	
+	public double calcularImposto() {
+		return this.rendaAnual * this.getAliquota();
+	}
 }
