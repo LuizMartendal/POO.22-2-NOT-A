859a7d14069b679783e299464f242e820bb82941
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2022-09-19 22:00:18 -0300

    Lista 3 - Exerc 5 (início)

---
+++ Lista3A-Exe5/src/Vereador.java
@@ -0,0 +1,78 @@
+
+public class Vereador {
+	// atributos
+	private String nome;
+	private Partido partido;
+	private int qtdProjApres, qtdProjAprov;
+
+	// construtor
+	public Vereador(String nome, int qtdProjApres, int qtdProjAprov) {
+		super();
+		this.setNome(nome);
+		this.setQtdProjApres(qtdProjApres);
+		this.setQtdProjAprov(qtdProjAprov);
+	}
+
+	// métodos
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		if (nome != null && !nome.isBlank()) {
+			this.nome = nome;
+		}
+	}
+
+	public Partido getPartido() {
+		return partido;
+	}
+
+	public void setPartido(Partido partido) {
+		this.partido = partido;
+	}
+
+	public int getQtdProjApres() {
+		return qtdProjApres;
+	}
+
+	public void setQtdProjApres(int qtdProjApres) {
+		if (qtdProjApres >= 0) {
+			this.qtdProjApres = qtdProjApres;
+		}
+	}
+
+	public int getQtdProjAprov() {
+		return qtdProjAprov;
+	}
+
+	public void setQtdProjAprov(int qtdProjAprov) {
+		if (qtdProjAprov >= 0) {
+			this.qtdProjAprov = qtdProjAprov;
+		}
+	}
+	
+	public double getDesempenho() {
+		if (this.qtdProjApres == 0) {
+			return 0;
+		}
+		return (this.qtdProjAprov / this.qtdProjApres) * this.getIndiceTrabalho();
+	}
+
+	public double getIndiceTrabalho() {
+		if (this.qtdProjApres > 17) {
+			return 1.22;
+		}
+		if (this.qtdProjApres > 10) {
+			return 1.08;
+		}
+		if (this.qtdProjApres > 5) {
+			return 1.0;
+		}
+		if (this.qtdProjApres > 0) {
+			return 0.8;
+		}
+		return 0;
+	}
+
+}
